fbbfc0420bd98d798e7489d1d799fcc243e854bd
Miguel Breceda de Lara <100646882+mbreceda@users.noreply.github.com>
2025-01-21 11:55:09 -0600

    Add Vehicles to API

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from inventory.models import Vehicle
+from .serializers import VehicleSerializer
 
-# Create your views here.
+
+class VehicleViewSet(viewsets.ModelViewSet):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
