a834d204512c32f9f62d00ea733d409b1e0e6b0d
mgbld <mbreceda@altimetrik.com>
2025-01-24 12:49:44 -0600

    Generate Pages

--- dealership/urls.py
+++ dealership/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('pages.urls')),
     path('', include('accounts.urls')),
     path('admin/', admin.site.urls),
     path('v1/portal/', include('api.urls')),
